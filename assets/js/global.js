5c16d0568a930a4c02b0a01891b8be635bffa2b4
Danish <dan71ish@gmail.com>
2023-02-19 20:18:28 +0530

    working on project

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,14 +1,15 @@
-// 'use strict'
+'use strict'
 
 // Add even on multiple element
-const addEvenonElements = function(elements, eventType, callback){
+const addEventOnElements = function(elements, eventType, callback){
     for(const elem of elements)elem.addEventListener(eventType,callback);
 }
 
 // Toggle search box in mobile device || small screen
 
 const searchBox = document.querySelector("[search-box]");
-const searchToggler = document.querySelector("[search-toggler]");
-addEvenonElements(searchToggler,"click",function(){
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+addEventOnElements(searchTogglers,"click",function(){
     searchBox.classList.toggle("active");
 })
+
