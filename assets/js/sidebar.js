2b089fa468337c58e56fec6a2e811c7e1a940fc1
Danish <dan71ish@gmail.com>
2023-02-23 08:18:54 +0530

    sidebar issue solved

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -51,18 +51,12 @@ export function sidebar(){
     const toggleSidebar = function(sidebar){
         // Toggle sidebar in only mobile screen
         const sidebarBtn = document.querySelector("[menu-btn]")
-        const sidebarTogglers = document.querySelectorAll("[menu-toggler]")
-        const sidebarClose = document.querySelectorAll("[menu-close]");
+        const sidebarTogglers = document.querySelectorAll("[menu-toggler]");
         const overlay = document.querySelector("[overlay]")
         addEventOnElements(sidebarTogglers, "click", function(){
             sidebar.classList.toggle("active");
             sidebarBtn.classList.toggle("active");
             overlay.classList.toggle("active");
-        });
-        addEventOnElements(sidebarClose, "click", function(){
-            sidebar.classList.remove("active");
-            sidebarBtn.classList.remove("active");
-            overlay.classList.remove("active");
         });
     }
 }
